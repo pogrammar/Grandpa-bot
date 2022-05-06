e2f4f904147ab3e90d404eb8913fc4ffc60abe14
Pogrammar <97349629+pogrammar@users.noreply.github.com>
2022-05-06 10:27:55 +0530

    Update aki.py

--- cogs/GameHandler/ButtonHandlers/aki.py
+++ cogs/GameHandler/ButtonHandlers/aki.py
@@ -18,8 +18,6 @@ class AkiView(discord.ui.View):
         if game.aki.progression >= game.win_at:
                 embed = await game.win()
                 await interaction.response.edit_message(embed=embed, view=self)
-                if answer == "yes":
-                    return self.stop()
         
         if interaction.user != game.player:
             return await interaction.response.send_message(content="This isn't your game")
